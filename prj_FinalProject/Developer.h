b7a5523e344528b081f42e61fff57c7bee6e9bdb
Nonne07 <98527083+Nonne07@users.noreply.github.com>
2026-03-13 08:40:26 +0700

    put varibles from publc to private

--- prj_FinalProject/Developer.h
+++ prj_FinalProject/Developer.h
@@ -6,19 +6,35 @@
 using namespace std;
 
 class Developer {
-public:
+private:
     string devID;
     string name;
     int birthYear;
     string languages;
     double salary;
 
+public:
     Developer();
     Developer(string, string, int, string, double);
+
+//Getters
+    string getDevID()     const;
+    string getName()      const;
+    int    getBirthYear() const;
+    string getLanguages() const;
+    double getSalary()    const;
+
+//Setters
+    void setDevID    (string devID);
+    void setName     (string name);
+    void setBirthYear(int birthYear);
+    void setLanguages(string languages);
+    void setSalary   (double salary);
 
     void input();
     void display();
     string toString();
 };
+
 
 #endif
