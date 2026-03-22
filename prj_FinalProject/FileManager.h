ff670063999dce5c4b235f36ea263610abdbb116
ldhl2405-pixel <ldhl2405@gmail.com>
2026-03-22 23:36:58 +0700

    Refactor FileManager to handle CSV operations

--- prj_FinalProject/FileManager.h
+++ prj_FinalProject/FileManager.h
@@ -2,18 +2,13 @@
 #define FILEMANAGER_H
 
 #include <vector>
+#include <string>
 #include "Developer.h"
-#include "Project.h"
 
 class FileManager {
 public:
-
-    static void saveDevelopers(vector<Developer>&);
-    static void loadDevelopers(vector<Developer>&);
-
-    static void saveProjects(vector<Project>&);
-    static void loadProjects(vector<Project>&);
-
+    static void saveToCSV(const std::vector<Developer>& list, const std::string& filename);
+    static void loadFromCSV(std::vector<Developer>& list, const std::string& filename);
 };
 
 #endif
