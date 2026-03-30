c88c83cd06694fe556532053140b778e97ac057d
Nonne07 <98527083+Nonne07@users.noreply.github.com>
2026-03-30 15:33:06 +0700

    Update Validator.h

--- prj_FinalProject/Validator.h
+++ prj_FinalProject/Validator.h
@@ -1,19 +1,17 @@
-#pragma once
-#include <string>
-
 class Validator {
+private:
+    bool isNumber(const std::string& s); // only used internally by other validators
+
 public:
-    static bool isValidDevID(const std::string& id);
-    static bool isValidProjectID(const std::string& id);
+    bool isValidDevID(const std::string& id);
+    bool isValidProjectID(const std::string& id);
 
-    static bool isNumber(const std::string& s);
+    bool isValidName(const std::string& name);
+    bool isValidBirthYearFormat(const std::string& yearStr);
+    bool isValidBirthYear(int year);
+    bool isValidSalary(double salary);
+    bool isValidLanguages(const std::string& lang);
 
-    static bool isValidName(const std::string& name);
-    static bool isValidBirthYearFormat(const std::string& yearStr);
-    static bool isValidBirthYear(int year);
-    static bool isValidSalary(double salary);
-    static bool isValidLanguages(const std::string& lang);
-
-    static bool isValidDuration(int duration);
-    static bool isValidStartDate(const std::string& date);
+    bool isValidDuration(int duration);
+    bool isValidStartDate(const std::string& date);
 };
